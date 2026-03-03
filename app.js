cb8661531d59c474996594eab3bdee3327c4323f
vivienne73 <moon070364@gmail.com>
2026-03-03 12:43:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const text = "soft pink personal space";
+let i = 0;
+function typing(){
+  if(i < text.length){
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing,50);
+  }
+}
+typing();
 
+document.addEventListener("mousemove",function(e){
+  const halo=document.querySelector(".halo");
+  halo.style.left=e.clientX-150+"px";
+  halo.style.top=e.clientY-150+"px";
+});
